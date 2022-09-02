a4eb477a525bbe54ab8f458707a8f44451efda48
Zoeyyy14 <70824285+Zoeyyy14@users.noreply.github.com>
2022-09-01 21:56:01 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,33 @@
+AFRAME.registerComponent("game",{
+    schema:{
+        gameState:{type:"string", default:"play"}
+    },
+    init:function(){
+        var duration=300
+        var timerEl=document.querySelector("#timer")
+        this.startTimer(duration,timerEl)
+    },
+    startTimer:function(duration, timerEl){
+        var minutes
+        var seconds
+        setInterval(()=>{
+            if(duration>=0){
+                this.data.gameState="play"
+                minutes=parseInt(duration/60)
+                seconds=parseInt(duration%60)
+
+                if(minutes<10){
+                    minutes="0"+minutes
+                }
+                if(seconds<10){
+                    seconds="0"+seconds
+                }
+                timerEl.setAttribute("text",{
+                    value:minutes+":"+seconds
+                })
+                duration-=1
+            }
+
+        })
+    }
+})
